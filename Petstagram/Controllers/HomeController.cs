fc40c35a99b00e15d1f594f4bbe8226e9f99f910
Max Rauchman <max.l.rauchman@gmail.com>
2023-05-05 13:15:09 -0400

    Session Lecture

--- Petstagram/Controllers/HomeController.cs
+++ Petstagram/Controllers/HomeController.cs
@@ -32,16 +32,45 @@ public class HomeController : Controller
         {
             return View("Index");
         }
-
-        
+  
         Console.WriteLine("Name: " + newPet.Name);
         Console.WriteLine("Type: " + newPet.Type);
         Console.WriteLine("Age: " + newPet.Age);
         Console.WriteLine("Color: " + newPet.Color);
 
+        HttpContext.Session.SetString("PetName", newPet.Name);
+        HttpContext.Session.SetString("PetType", newPet.Type);
+        if(newPet.Color != null)
+        {
+            HttpContext.Session.SetString("PetColor", newPet.Color);
+        }
+        if(newPet.Age != null)
+        {
+            HttpContext.Session.SetInt32("PetAge", (int)newPet.Age);
+        }
+
+        string? sessionName = HttpContext.Session.GetString("PetName");
+        int? sessionAge = HttpContext.Session.GetInt32("PetAge");
+
+        Console.WriteLine(sessionName);
+        Console.WriteLine(sessionAge);
+
         // we're not redirecting here b/c we're not working with a DB yet, typically this would
         // be a redirect
-        return View("ViewPet", newPet);
+        return Redirect("ViewPet");
+    }
+
+    [HttpGet("/viewPet")]
+    public IActionResult ViewPet()
+    {
+        return View("ViewPet");
+    }
+
+    [HttpPost("/clearSession")]
+    public IActionResult ClearSession()
+    {
+        HttpContext.Session.Clear();
+        return Redirect("/");
     }
 
     [HttpGet("/privacy")]
--- Petstagram/Program.cs
+++ Petstagram/Program.cs
@@ -2,6 +2,9 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+// Services to be able to use Session, must come before the builder.Build() line
+builder.Services.AddHttpContextAccessor();  
+builder.Services.AddSession();  
 
 var app = builder.Build();
 
@@ -10,11 +13,11 @@ if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
 }
+
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthorization();
+app.UseSession(); 
 
 app.MapControllerRoute(
     name: "default",
